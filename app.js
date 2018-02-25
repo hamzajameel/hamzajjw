92752af4ef049428a9a85f8b8b26d57cbb18b00c
hamzajameel <prince24780@gmail.com>
2018-02-25 19:34:18 +0500

    Practise Git

---
+++ app.js
@@ -0,0 +1,190 @@
+// Alert Assaingment 1
+// var a = "Error! Please enter a valid password";
+// alert(a)
+
+// Alert Assaingment 2
+
+// var a = "Welcom to JS Land...\n Happy Coding!";
+// alert (a);
+
+// Alert Assaingment 3
+//  var a = "Hello...I can run JS through we Browser's console";
+//  alert (a);
+// console.log(a);
+
+// VARIABLES FOR STRINGS Assaingment 1
+
+// var a = "Hamza Ahmed";
+// var b = "17 years old";
+// var c = "SSUET";
+// var d = "Mobile & Clound Computing - Module A";
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+
+// VARIABLES FOR STRINGS Assaingment 2
+
+// var a = " PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(a);
+
+// VARIABLES FOR STRINGS Assaingment 3
+
+// var a = "You will be learning JS In Saylani In 2050 With Your Kids";
+// alert(a);
+
+// VARIABLES FOR STRINGS Assaingment 4
+
+// var a = prompt(" Enter your email");
+// var b = " You email is " + a;
+// alert(b);
+
+
+// VARIABLES FOR STRINGS Assaingment 5
+
+// var a = "I am trying to learn from the book A smarter way to learn JavaScript"
+// alert(a);
+
+// var a = "My name is Ali Khan \n I am 17 years old \n I study in SSUET \n I take Mobile & Cloud Computing, Module A classes \n";
+// document.write(a);
+
+// USER	INPUT	&	
+// CONDITIONAL	STATEMENTS Assaingment 1
+// var num = prompt("Enter Number", "0") 
+// var num = parseInt(num); 
+// var i = 0;
+ 
+// document.write('<table border="1" cellspacing="0">');
+// for(i=1;i<10;i++) {
+//     document.write("<tr><td>" + num + " x " + i + " = " + num*i + "</td></tr>");
+// }
+ 
+// document.write("</table>");
+
+// USER	INPUT	&	
+// CONDITIONAL	STATEMENTS Assaingment 2
+
+// var a =prompt("Enter you city");
+// if ( a === "Karachi") {
+//     alert ("Welcome to karachi")
+// }
+// else {
+//     alert ("Not A City")
+// }
+
+// var a = prompt("Enter your gender");
+// if (a === "male") {alert 
+//     ("Good mornig Sir")
+// }
+// else if (a === "female"){
+//     alert("Good Mornig Madam")
+// }
+// else {
+//     alert("Please Input Gender")
+// }
+
+// USER	INPUT	&	
+// CONDITIONAL	STATEMENTS Assaingment 3
+
+// var a = prompt("Enter Colour");
+// if (a === red){alert
+//     ("vehicles must stop")
+// }
+// if (a === green){
+//     alert("vehicles can move now")
+// }
+// if (a === yellow){
+//     alert("vehicles should get ready to move")
+// }
+// else{
+//     alert("Not a Colour")
+// }
+
+// USER	INPUT	&	
+// CONDITIONAL	STATEMENTS Assaingment 4
+
+// var c = prompt("Enter your age");
+// var a = 1
+// var b = 100
+// if ( c > a || c < b){
+//     alert ("you are welcome")
+// }
+
+// IF…ELSE	&	ELSE	IF	STATEMENTS,	
+// TESTING	SET	OF	CONDITIONS &	
+// SWITCH	STATEMENTS Assaingment 1
+
+// var n =+prompt("Enter your number");
+// if (n % 3 == 0){
+//     alert("this is divisible by")
+// }
+// else {alert("Nan")}
+
+// IF…ELSE	&	ELSE	IF	STATEMENTS,	
+// TESTING	SET	OF	CONDITIONS &	
+// SWITCH	STATEMENTS Assaingment 2
+
+// var n =+prompt("Enter your number");
+// if (n % 2 == 0){
+//     alert("Number is even")
+// }
+// else {alert("Odd Number")}
+
+// IF…ELSE	&	ELSE	IF	STATEMENTS,	
+// TESTING	SET	OF	CONDITIONS &	
+// SWITCH	STATEMENTS Assaingment 3
+
+// var a = +prompt("enter your age");
+// if ( a > 18 ===18 ) { 
+//     alert("old enough")
+// }
+// else {
+//     alert("Too young")
+// }
+
+// IF…ELSE	&	ELSE	IF	STATEMENTS,	
+// TESTING	SET	OF	CONDITIONS &	
+// SWITCH	STATEMENTS Assaingment 4
+
+// var a = prompt("Enter your name");
+// if (a === "hamza"){alert("welcome")}
+// else {alert()}
+
+// IF…ELSE	&	ELSE	IF	STATEMENTS,	
+// TESTING	SET	OF	CONDITIONS &	
+// SWITCH	STATEMENTS Assaingment 5
+
+// var a = 10;
+// if (a !== 10 ){alert ("true")}
+// else {("false")}
+
+// IF…ELSE	&	ELSE	IF	STATEMENTS,	
+// TESTING	SET	OF	CONDITIONS &	
+// SWITCH	STATEMENTS Assaingment 6
+
+// var firstName = "Ali";
+// if (firstName === "Fahad") {
+//  document.write("Hello Fahad!");
+// } else {alert("You are failed")}
+
+
+//   MATH EXPRESSIONS 8
+
+// var b = +prompt("Enter your number");
+// var a = 550;
+// var c = (b / 550) * 100;
+// var d = Math.round(c);
+// document.write(d);
+
+//   MATH EXPRESSIONS 9
+
+// var dollar = 104;
+// var riyal = 28;
+// var a = +prompt("enter dollars");
+// var d = +prompt("enter riyar");
+// var e = (104*a);
+// var r = (28*d);
+// var t = e + r;
+// document.write(e);
+// document.write(r);
+// document.write(t);
